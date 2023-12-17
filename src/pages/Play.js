6b076562363dca0a123500809e4d929a24123550
Lucas Pereira <lucasferrari4@gmail.com>
2023-12-17 13:23:06 -0700

    day12

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,5 +1,14 @@
+import { Unity, useUnityContext } from "react-unity-webgl";
+
 const Play = () => {
-    return <h1>Game</h1>;
+  const { unityProvider } = useUnityContext({
+    loaderUrl: "build_game/_BuildWeb.loader.js",
+    dataUrl: "build_game/_BuildWeb.data",
+    frameworkUrl: "build_game/_BuildWeb.framework.js",
+    codeUrl: "build_game/_BuildWeb.wasm",
+  });
+
+  return <Unity unityProvider={unityProvider} />;
   };
   
   export default Play;
